0503aef6edb8bceb03646910e85c996925ded06f
Alexey <tihiy-ti@mail.ru>
2024-10-27 00:15:45 +0300

    To 22 java migrate

---
+++ module1/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.ti {
+//    requires javafx.controls;
+//    requires javafx.fxml;
+
+//    requires org.controlsfx.controls;
+//    requires org.kordamp.bootstrapfx.core;
+
+//    opens com.example.demo1 to javafx.fxml;
+    exports com.ti;
+}
